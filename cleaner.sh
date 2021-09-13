d43e3920efd6563c203d6d290f7e294fb6e53f89
Ekylate <jt.antoine@gmail.com>
2021-09-13 22:40:41 +0200

    Create cleaner.sh
    Init of path to the file of the books' names

---
+++ cleaner.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Récupération des noms de livres en cours..."
+pathToLibrary='~/Documents/ProPresenter/Libraries/Default'
+
+#TODO:jta : update path asap
+fileContent=`cat $pathToLibrary`
+
+#echo $fileContent | tr 
