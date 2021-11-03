010f3eb1bc26db005eee3057123cea85a21bfda5
Ekylate <jt.antoine@gmail.com>
2021-11-03 20:21:42 +0100

    files to be deleted file creation
    [JTA] : #5 : Creation of the file containing all the files to delete

--- cleaner.sh
+++ cleaner.sh
@@ -5,6 +5,8 @@ cleanerResourcesFolder=$personalDir'/Documents/Tools4PP/Cleaner/'
 outputFolder=$cleanerResourcesFolder'/outputs'
 inputFolder=$cleanerResourcesFolder'/inputs'
 pathToLibrary=$personalDir'/Documents/ProPresenter/Libraries/Default'
+shortcutsFile=$outputFolder'/'$launchingTime'-shortcuts.txt'
+listToDeleteFiles=$outputFolder'/'$launchingTime'-filesToDelete.txt'
 
 echo 'Récupération des noms de livres en cours...'
 echo 'Création filtres en fonction des noms de livres en cours...'
@@ -18,7 +20,8 @@ echo '$pathToFileFilter = '$pathToFileFilter
 # echo '$nbFileLines = '$nbFileLines
 # echo '$nbContentLines = '`echo $fileContent | wc -l`
 
-cat $pathToFileFilter | cut -c 1-4 > $outputFolder'/'$launchingTime'-shortcuts.txt'
+cat $pathToFileFilter | cut -c 1-4 > $shortcutsFile
 
-#echo $fileContent | tr 
- 
+#Double arrow to append or create if inexistant
+##TODO : populate woth correct values
+cat ' ' >> $listToDeleteFiles
